57435625836b57cea72c1c4074a11b13d67f1478
Louis Rubet <louis@rubet.fr>
2016-01-06 19:11:34 +0100

    First commit

---
+++ app/src/main/java/com/example/louis/kami_like/ClassicPuzzlesActivity.java
@@ -0,0 +1,14 @@
+package com.example.louis.kami_like;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ClassicPuzzlesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classic_puzzles);
+    }
+}
---
+++ app/src/main/java/com/example/louis/kami_like/InfoActivity.java
@@ -0,0 +1,24 @@
+package com.example.louis.kami_like;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class InfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_info);
+    }
+
+    /** info */
+    public void onClickInfoOk(View view) {
+        finish();
+    }
+}
