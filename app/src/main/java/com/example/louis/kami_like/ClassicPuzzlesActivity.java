e48512ce9a8e07a8e107b1523bfeb72b6f045765
louis <louis@rubet.fr>
2016-01-07 17:34:06 +0100

    Test change

--- app/src/main/java/com/example/louis/kami_like/ClassicPuzzlesActivity.java
+++ app/src/main/java/com/example/louis/kami_like/ClassicPuzzlesActivity.java
@@ -3,12 +3,21 @@ package com.example.louis.kami_like;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class ClassicPuzzlesActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_classic_puzzles);
+    }
+
+    /** info */
+    public void onClickClassic1(View view)
+    {
+        finish();
     }
 }
